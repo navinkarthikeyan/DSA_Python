b91f07aa8397059f15808b6986f1b21c0b592bc0
Navin Karthikeyan <52325703+navinkarthikeyan@users.noreply.github.com>
2023-04-27 17:45:02 +0530

    Delete access an element in array.py

--- access an element in array.py
+++
@@ -1,8 +0,0 @@
-from array import *
-arr1 = array('i',[1,2,3,4,5,6])
-def accessElement(array,index):
-    if index >= len(array):
-        print("Not a valid input")
-    else:
-        print(array[index])
-accessElement(arr1,4)

89aadae1330b5c0626d89edf16c480ce000b1bbe
Navin Karthikeyan <52325703+navinkarthikeyan@users.noreply.github.com>
2023-04-27 20:17:58 +0530

    Add files via upload

---
+++ ArraysList/Mean_of_an_array.py
@@ -0,0 +1,8 @@
+myList = list()
+while(True):
+    inp = input('Enter a number: ')
+    if inp == 'done' : break
+    value = float(inp)
+    myList.append(value)
+average = sum(myList) / len(myList)
+print('Average:' , average)

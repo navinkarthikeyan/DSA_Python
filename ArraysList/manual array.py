182a20c264b333f4aa5be7daabfd83285a8e19e3
Navin Karthikeyan <52325703+navinkarthikeyan@users.noreply.github.com>
2023-04-27 17:44:29 +0530

    Add files via upload

---
+++ ArraysList/manual array.py
@@ -0,0 +1,18 @@
+lst = [] 
+
+  
+# number of elements as input 
+
+n = int(input("Enter number of elements : ")) 
+
+  
+# iterating till the range 
+
+for i in range(0, n): 
+
+    ele = int(input()) 
+
+    # adding the element 
+
+    lst.append(ele)
+    print(lst)
